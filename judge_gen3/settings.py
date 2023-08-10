0e62368d256fd0144ead76469bac445de09a4b2a
ijknabla <ijknabla@gmail.com>
2023-08-10 18:20:35 +0900

    Add /templates/ to judge_gen3.settings

--- judge_gen3/settings.py
+++ judge_gen3/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = "judge_gen3.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

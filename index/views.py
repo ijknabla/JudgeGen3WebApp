1e4b27ebc9536fa59515cd1dcc5e1f26fcbd8345
ijknabla <ijknabla@gmail.com>
2023-08-10 19:01:18 +0900

    Add POST handle

--- index/views.py
+++ index/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        if "pokemon_button" in request.POST:
+            ...
     return render(request, "index.html")
